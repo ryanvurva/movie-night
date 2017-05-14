21304d95adb47dc37485c87ec7ce8b28e635ab35
Ryan Vurva <ryanvurva@gmail.com>
2017-05-14 11:10:16 -0400

    browse idea

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -16,6 +16,20 @@ class Browse extends Component {
           <li><NavLink to='/browse/0'>#</NavLink></li>
         </ul>
       </div>
+      <div className='letterSelector'>
+        <select name='browse' id='browse'>
+          {letters.map((l) => (
+            <option key={l}>
+              {/* need to resolve this issue */}
+              {l}
+            </option>
+          ))}
+          {/* <option value='value1'>Value 1</option>
+          <option value='value2'>Value 2</option>
+          <option value='value3'>Value 3</option> */}
+        </select>
+        <button><i className='fa fa-search' aria-hidden='true' /></button>
+      </div>
     </div>
   }
 }
