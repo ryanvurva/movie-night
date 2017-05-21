67c7358f49eb21b4cc616e3809e38e0f405aedc4
Ryan Vurva <ryanvurva@gmail.com>
2017-05-21 00:46:55 -0400

    worked on review cards
    need to resolve border bottom issue for review cards at certain widths

--- src/components/Home.js
+++ src/components/Home.js
@@ -123,12 +123,24 @@ class Home extends Component {
           <NavLink to='/reviews'>...see all</NavLink>
         </div>
         <div className='Home-content'>
-          <div className='Home-review-section'>
-            <div className='Critic'>
-              <NavLink to='/:name'>someGuy123's</NavLink> opinion about 'Some Movie':
+          <div className='Home-review-card'>
+            <div className='Home-review-section'>
+              <div className='Critic'>
+                <NavLink to='/:name'>someGuy123's</NavLink> opinion about 'Some Movie':
+              </div>
+              <div className='Opinion'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
             </div>
-            <div className='Opinion'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div className='Home-review-card'>
+            <div className='Home-review-section'>
+              <div className='Critic'>
+                <NavLink to='/:name'>someGuy123's</NavLink> opinion about 'Some Movie':
+              </div>
+              <div className='Opinion'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -144,20 +144,34 @@ class Profile extends Component {
             <NavLink to='#'>...see all</NavLink>
           </div>
           <div className='Home-content'>
-            <div className='Home-review-section'>
-              <div className='Critic'>
-                <NavLink to='/:name'>Ryan Vurva's</NavLink> opinion about 'Latin Words':
-              </div>
-              <div className='Opinion'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className='reviewCard'>
+              <div className='review-section'>
+                <div className='Critic'>
+                  <NavLink to='/:name'>Ryan Vurva's</NavLink> opinion about 'Latin Words':
+                </div>
+                <div className='Opinion'>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
               </div>
             </div>
-            <div className='Home-review-section'>
-              <div className='Critic'>
-                <NavLink to='/:name'>Ryan Vurva's</NavLink> opinion about 'Latin Words':
+            <div className='reviewCard'>
+              <div className='review-section'>
+                <div className='Critic'>
+                  <NavLink to='/:name'>Ryan Vurva's</NavLink> opinion about 'Latin Words':
+                </div>
+                <div className='Opinion'>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
               </div>
-              <div className='Opinion'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div className='reviewCard'>
+              <div className='review-section'>
+                <div className='Critic'>
+                  <NavLink to='/:name'>Ryan Vurva's</NavLink> opinion about 'Latin Words':
+                </div>
+                <div className='Opinion'>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
               </div>
             </div>
           </div>
