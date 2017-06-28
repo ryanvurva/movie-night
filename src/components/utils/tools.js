67d0a0edb79f762c49cba32a107df2c948933ad6
Ryan Vurva <ryanvurva@gmail.com>
2017-06-28 14:56:59 -0400

    check

---
+++ src/components/utils/tools.js
@@ -0,0 +1,19 @@
+// import { jQuery } from 'jquery'
+//
+// jQuery(document).ready(function () {
+//   'use strict'
+//   scrollAnnouncement()
+//
+//   function scrollAnnouncement () {
+//     jQuery('.announcement-inner li.listing-item').delay(2000).animate(
+// {left: '-=' + (jQuery('.announcement-inner li.listing-item').width() - jQuery('.announcement-inner .display-posts-listing').width()) + 'px'},
+// 100 * 1000, function () {
+//   jQuery(this).delay(5000).fadeOut(1000, function () {
+//     jQuery(this).css('left', '0').fadeIn(1000, function () {
+//       scrollAnnouncement()
+//     })
+//   })
+// }
+// )
+//   }
+// })
